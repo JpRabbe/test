c0d2789099b49246ca3661a159e364f7314f8e95
JpRabbe <87547679+JpRabbe@users.noreply.github.com>
2022-12-14 13:09:33 +0100

    Add files via upload

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,6 +13,7 @@ describe('form testing', () => {
     cy.get('input[aria-label="Nombre de chambre"]').clear().type('2.00');
     cy.get('input[aria-label="Y a un jardin"]').clear().type('0.00'); 
 
-    cy.get('#prediction').should('exist');
+    // cy.get('#prediction').should('exist');
+    cy.get('#pr-diction-de-prix-de-maison').should('exist');
   })
 })
