f29b33454e4bf89fc12e5ed2cd3a4f38b6c14d9f
JpRabbe <87547679+JpRabbe@users.noreply.github.com>
2022-12-14 12:57:52 +0100

    Add files via upload

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:8501/')
+    cy.get('#pr-diction-de-prix-de-maison').should('exist');
+  })
+})
+
+describe('form testing', () => {
+  it('should submit the form with valid input', () => {
+    cy.visit('http://localhost:8501/')
+
+    cy.get('input[aria-label="Taille maison"]').clear().type('260.00');
+    cy.get('input[aria-label="Nombre de chambre"]').clear().type('2.00');
+    cy.get('input[aria-label="Y a un jardin"]').clear().type('0.00'); 
+
+    cy.get('#prediction').should('exist');
+  })
+})
